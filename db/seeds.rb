9859f909aa31284f16befeae6764871a13261aeb
jnarowski <jnarowski@gmail.com>
2019-08-20 14:48:03 -0600

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,18 @@
 # ActiveRecord::Base.connection.execute("TRUNCATE stats RESTART IDENTITY")
 
-(Date.new(2010, 1, 1)..Date.today).each do |date|
+number_of_websites = 5
+number_of_webpages = 1000
+start_date = Date.new(2010, 1, 1)
+end_date = Date.today
+ 
+(start_date..end_date).each do |date|
   stats = []
   p "........................................"
   p "[INFO] date: #{date}"
-  1.upto(100) do |website_id|
+  1.upto(number_of_websites) do |website_id|
     p "........................................"
     p "[INFO] website_id: #{website_id} #{date}"
-    1.upto(1000) do |webpage_id|
+    1.upto(number_of_webpages) do |webpage_id|
       stats << {
         date: date,
         clicks: rand(1..100),
